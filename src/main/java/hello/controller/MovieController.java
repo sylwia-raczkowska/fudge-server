75d07b7e0bc534f6716ea783eb61e456732af6f9
hojam <michal.hoja@student.put.poznan.pl>
2018-04-18 19:00:17 +0200

    Add endpoint for movie ratings and movie details.

--- src/main/java/hello/controller/MovieController.java
+++ src/main/java/hello/controller/MovieController.java
@@ -1,24 +1,50 @@
 package hello.controller;
 
 import hello.model.movielens.Movie;
+import hello.model.movielens.Rating;
 import hello.repository.MovieRepository;
+import hello.repository.RatingsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
     private MovieRepository movieRepository;
+    private RatingsRepository ratingsRepository;
 
     @Autowired
-    public MovieController(MovieRepository movieRepository) {
+    RestTemplate restTemplate;
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    public MovieController(MovieRepository movieRepository, RatingsRepository ratingsRepository) {
         this.movieRepository = movieRepository;
+        this.ratingsRepository = ratingsRepository;
     }
 
     @RequestMapping("/{movieId}")
     public Movie getMovie(@PathVariable Integer movieId) {
         return movieRepository.findOne(movieId);
+    }
+
+    @RequestMapping(value = "/{movieId}/details", produces = "application/json")
+    public String getMovieDetails(@PathVariable Integer movieId) {
+        String apiKey = env.getProperty("omdb.key");
+        String imdbId = getMovie(movieId).getLinks().getImdbId();
+        return restTemplate.getForObject("http://www.omdbapi.com/?i=tt0" + imdbId + "&apikey=" + apiKey, String.class);
+    }
+
+    @RequestMapping("/{movieId}/ratings")
+    public List<Rating> getRatings(@PathVariable Integer movieId) {
+        return ratingsRepository.findAllByRatingKey_MovieId(movieId);
     }
 }

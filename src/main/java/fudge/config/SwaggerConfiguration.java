c25746e64bd963d9163e8fffe6bbb2c6353726c5
sucuncu <sucuncu@consdata.pl>
2018-06-26 07:40:14 +0200

    swagger

--- src/main/java/fudge/config/SwaggerConfiguration.java
+++ src/main/java/fudge/config/SwaggerConfiguration.java
@@ -1,4 +1,4 @@
-package hello.config;
+package fudge.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/fudge/config/WebMvcConfig.java
+++ src/main/java/fudge/config/WebMvcConfig.java
@@ -2,6 +2,8 @@ package fudge.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration

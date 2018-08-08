a16c406ae0b5b767590f2f4088ccbd05f6d36c47
Michał H <michuu93@gmail.com>
2018-08-08 21:10:40 +0200

    top100 movies

--- src/main/java/fudge/repository/AverageRatingsRepository.java
+++ src/main/java/fudge/repository/AverageRatingsRepository.java
@@ -2,8 +2,13 @@ package fudge.repository;
 
 import fudge.model.rating.AverageRating;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AverageRatingsRepository extends JpaRepository<AverageRating, Integer> {
+    @Query("SELECT rating FROM AverageRating rating ORDER BY rating.averageRating DESC")
+    List<AverageRating> findTop100();
 }
--- src/main/java/fudge/service/MovieService.java
+++ src/main/java/fudge/service/MovieService.java
@@ -5,10 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface MovieService {
     Page<Movie> getMovies(Pageable pageable);
 
     Page<Movie> findMoviesByTitle(String title, Pageable pageable);
 
     ResponseEntity<Movie> getMovie(Integer movieId);
+
+    ResponseEntity<List<Movie>> getTop100Movies();
 }
